de8cc5a16cdca8b9f561b6c96033ea0ff68e4477
yanwei841112 <yanwei841112@gmail.com>
2016-08-08 14:16:01 +0800

    Add drawing tools

--- public/js/src/index.js
+++ public/js/src/index.js
@@ -45,7 +45,7 @@ function initMap() {
         };
 
         infoWindow.setPosition(pos);
-        infoWindow.setContent("Hey! I'm here!!");
+        infoWindow.setContent("Hey,I'm here!!");
         map.setCenter(pos);
       },
       () => {
@@ -68,6 +68,31 @@ function initMap() {
       radius: Math.sqrt(cityMap[city].population) * 20,
     });
   }
+    // Drawing tools
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_CENTER,
+      drawingModes: [
+        google.maps.drawing.OverlayType.MARKER,
+        google.maps.drawing.OverlayType.CIRCLE,
+        google.maps.drawing.OverlayType.POLYGON,
+        google.maps.drawing.OverlayType.POLYLINE,
+        google.maps.drawing.OverlayType.RECTANGLE,
+      ],
+    },
+    markerOptions: { icon: 'images/beachflag.png' },
+    circleOptions: {
+      fillColor: '#ffff00',
+      fillOpacity: 1,
+      strokeWeight: 5,
+      clickable: false,
+      editable: true,
+      zIndex: 1,
+    },
+  });
+  drawingManager.setMap(map);
 
   // Create the search box and link it to the UI element.
   const input = document.getElementById('pac-input');
